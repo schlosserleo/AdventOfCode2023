940007798b0738c45859daf3b71636ba2eb884ed
schlosserleo <gymnasium.ottweiler@gmail.com>
2023-12-03 02:50:36 +0100

    commit

--- Day1.py
+++ Day1.py
@@ -1,4 +1,4 @@
-inputFile = open("/home/leo/Documents/AdventOfCode/2023/Day1Input.txt", "r")
+inputFile = open("Day1Input.txt", "r")
 
 number_dict = {"one" : "1",
                "two" : "2",
@@ -11,27 +11,68 @@ number_dict = {"one" : "1",
                "nine" : "9"
                }
 
-sum = 0
-all_digits = ""
-number_as_string = ""
-first_last_digit = ""
+nums_as_words =["one", "two", "three","four", "five", "six", "seven", "eight", "nine"]
 
-for line in inputFile:
-    for x in line:
-        if x.isdigit():
-            all_digits += x
-        else:
-            number_as_string += x
-            if number_as_string in number_dict:
-                all_digits += number_dict[number_as_string]
-                number_as_string = ""
-    number_as_string = ""
+def find_index_num_as_word_left(string_to_search):
+    min_index = find_any_index(string_to_search)
+    num_as_word = "null"
+    for x in nums_as_words:
+        temp_index = string_to_search.find(x)
+        if temp_index != -1 and temp_index <= min_index:
+            min_index = temp_index
+            num_as_word = x
     
-    first_last_digit = all_digits[0] + all_digits[-1]
-    print(first_last_digit)
-    sum += int(first_last_digit)
-    first_last_digit = ""
-    all_digits = ""
+    return [min_index, num_as_word]
+
+def find_index_num_as_word_right(string_to_search):
+    max_index = find_any_index(string_to_search)
+    num_as_word = "null"
+    for x in nums_as_words:
+        temp_index = string_to_search.rfind(x)
+        if temp_index != -1 and temp_index >= max_index:
+            max_index = temp_index
+            num_as_word = x
+            
+    return [max_index, num_as_word]
+        
+def find_any_index(string_to_search):
+    count = 0
+    any_index = -1
+    while any_index == -1 and count < 9:
+        any_index = string_to_search.find(nums_as_words[count])
+        count += 1
+    
+    return any_index
+
+def smallest_biggest_num_as_char(string_to_search):
+    indices = []
+    for index, value in enumerate(string_to_search):
+        if value.isdigit():
+            indices.append(index)
+    indices.sort()
+    if indices == []:
+        return [-1]
+    return [indices[0], indices[-1]]
+
+def get_left_right(string_results, char_results, input_string):
+    if string_results[0][0] == -1:
+        return input_string[char_results[0]] + input_string[char_results[1]]
+    if char_results[0] == -1:
+        return number_dict[string_results[0][1]] + number_dict[string_results[1][1]]
+    if string_results[0][0] < char_results[0]:
+        left = number_dict[string_results[0][1]]
+    else:
+        left = input_string[char_results[0]]
+    if string_results[1][0] > char_results[1]:
+        right = number_dict[string_results[1][1]]
+    else:
+        right = input_string[char_results[1]]
+    return left + right
+
+
+sum = 0
+for line in inputFile:
+    sum += int(get_left_right([find_index_num_as_word_left(line),find_index_num_as_word_right(line)],smallest_biggest_num_as_char(line),line))
 
 print(sum)
 
--- test.py
+++ test.py
@@ -1,32 +1,32 @@
 
 number_dict = {"one" : "1","two" : "2","three" :"3","four" : "4","five" : "5","six" : "6","seven" : "7","eight" : "8","nine" : "9"}
 nums_as_words =["one", "two", "three","four", "five", "six", "seven", "eight", "nine"]
-
-important_string="rgxjrsldrfmzq25szhbldzqhrhbjpkbjlsevenseven"
-
-
-print("left: "+str(left_num)+"\nright: "+str(right_num))
-
+important_string="53sevenvvqm"
 
 def find_index_num_as_word_left(string_to_search):
     min_index = find_any_index(string_to_search)
+    num_as_word = "null"
     for x in nums_as_words:
         temp_index = string_to_search.find(x)
-        if temp_index != -1 and temp_index < min_index:
+        if temp_index != -1 and temp_index <= min_index:
             min_index = temp_index
-            
-    return min_index
+            num_as_word = x
+    
+    return [min_index, num_as_word]
 
 def find_index_num_as_word_right(string_to_search):
     max_index = find_any_index(string_to_search)
+    num_as_word = "null"
     for x in nums_as_words:
         temp_index = string_to_search.rfind(x)
-        if temp_index != -1 and temp_index > max_index:
+        if temp_index != -1 and temp_index >= max_index:
             max_index = temp_index
+            num_as_word = x
             
-    return max_index
+    return [max_index, num_as_word]
         
 def find_any_index(string_to_search):
+    count = 0
     any_index = -1
     while any_index == -1 and count < 9:
         any_index = string_to_search.find(nums_as_words[count])
@@ -40,5 +40,27 @@ def smallest_biggest_num_as_char(string_to_search):
         if value.isdigit():
             indices.append(index)
     indices.sort()
-    
+    if indices == []:
+        return [-1]
     return [indices[0], indices[-1]]
+
+def get_left_right(string_results, char_results, input_string):
+    if string_results[0][0] == -1:
+        return input_string[char_results[0]] + input_string[char_results[1]]
+    if char_results[0] == -1:
+        return number_dict[string_results[0][1]] + number_dict[string_results[1][1]]
+    if string_results[0][0] < char_results[0]:
+        left = number_dict[string_results[0][1]]
+    else:
+        left = input_string[char_results[0]]
+    if string_results[1][0] > char_results[1]:
+        right = number_dict[string_results[1][1]]
+    else:
+        right = input_string[char_results[1]]
+    return left + right
+
+left_string_result = find_index_num_as_word_left(important_string)
+right_string_result = find_index_num_as_word_right(important_string)
+string_results = [left_string_result,right_string_result]
+char_results = smallest_biggest_num_as_char(important_string)
+print(get_left_right(string_results, char_results, important_string))
